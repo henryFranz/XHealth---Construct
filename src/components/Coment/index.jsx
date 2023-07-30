d14bd56558d5b5b475bd8785c1f6502b5c6eb607
henryFranz <140767487+henryFranz@users.noreply.github.com>
2023-07-30 15:02:38 -0300

    adicionando a pagina de depoimentos

--- src/components/Coment/index.jsx
+++ src/components/Coment/index.jsx
@@ -3,7 +3,9 @@ import style from "../index.module.css"
 export default function Coment({text,src,name}){
     return <div className={style.cardComent}>
         <span className={style.textComent}>{text}</span>
-        <img className={style.imgComent} src={src} alt={name} />
+       <div className={style.datePerson}>
+       <img className={style.imgComent} src={src} alt={name} />
         <span className={style.nameComent}>{name}</span>
+       </div>
     </div>
 }
