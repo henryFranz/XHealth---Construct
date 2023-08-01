33ccde84c71289073a5d7a3b8be144d068ea24a1
Henry <franzhenry46@gmail.com>
2023-08-01 10:18:56 -0300

    Responsividade da tela de professores

--- src/pages/Professores.jsx
+++ src/pages/Professores.jsx
@@ -1,10 +1,8 @@
 import style from "./styles.module.css";
-import personalFuncional from"../assets/personais/P.F.H.jpg"
-import personalLuta from"../assets/personais/P.L.jpg"
-import personalMusculacao from"../assets/personais/P.M.jpg"
-import personalZumba from"../assets/personais/P.Z.jpg"
-
-
+import personalFuncional from "../assets/personais/P.F.H.jpg";
+import personalLuta from "../assets/personais/P.L.jpg";
+import personalMusculacao from "../assets/personais/P.M.jpg";
+import personalZumba from "../assets/personais/P.Z.jpg";
 
 export default function Professores() {
   return (
@@ -12,18 +10,22 @@ export default function Professores() {
       <main className={style.professores}>
         <span className={style.title}>PROFESSORES</span>
         <div className={style.personals}>
-          <span className={style.personal}> 
-          <span>Treinador</span>
-          <img src={personalFuncional} /></span>
-          <span className={style.personal}> 
-          <span>Treinador</span>
-          <img src={personalLuta} /></span>
-          <span className={style.personal}> 
-          <span>Treinador</span>
-          <img src={personalMusculacao} /></span>
-          <span className={style.personal}> 
-          <span>Treinador</span>
-          <img src={personalZumba} /></span>
+          <span className={style.personal}>
+            <span className={style.personalTitle}>Treinador</span>
+            <img src={personalFuncional} />
+          </span>
+          <span className={style.personal}>
+            <span className={style.personalTitle}>Treinador</span>
+            <img src={personalLuta} />
+          </span>
+          <span className={style.personal}>
+            <span className={style.personalTitle}>Treinador</span>
+            <img src={personalMusculacao} />
+          </span>
+          <span className={style.personal}>
+            <span className={style.personalTitle}>Treinador</span>
+            <img src={personalZumba} />
+          </span>
         </div>
       </main>
     </>
