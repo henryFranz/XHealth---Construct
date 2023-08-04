4d69682898084d252f1a9acefe59b30345674900
henryFranz <140767487+henryFranz@users.noreply.github.com>
2023-08-03 22:16:12 -0300

    Update index.jsx

--- src/components/Galery/index.jsx
+++ src/components/Galery/index.jsx
@@ -1,10 +1,10 @@
-import image1 from "../../assets/image1.png";
-import image2 from "../../assets/image2.png";
-import image3 from "../../assets/image3.png";
-import image4 from "../../assets/image4.png";
-import image5 from "../../assets/image5.png";
-import image6 from "../../assets/image6.png";
-import image7 from "../../assets/image7.png";
+import image1 from "../assets/image1.png";
+import image2 from "../assets/image2.png";
+import image3 from "../assets/image3.png";
+import image4 from "../assets/image4.png";
+import image5 from "../assets/image5.png";
+import image6 from "../assets/image6.png";
+import image7 from "../assets/image7.png";
 
 import { Link } from "react-router-dom";
 import style from "../index.module.css";
@@ -82,3 +82,4 @@ export default function Galery() {
     </>
   );
 }
+
